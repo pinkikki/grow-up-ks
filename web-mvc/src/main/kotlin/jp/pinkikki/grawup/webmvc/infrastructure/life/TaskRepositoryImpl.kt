16257b9f840a01a0dc79a421581251beb4fedcde
pinkikki <w-mizutani@pinkikki.jp>
2020-05-24 00:12:29 +0900

    chore

--- web-mvc/src/main/kotlin/jp/pinkikki/grawup/webmvc/infrastructure/life/TaskRepositoryImpl.kt
+++ web-mvc/src/main/kotlin/jp/pinkikki/grawup/webmvc/infrastructure/life/TaskRepositoryImpl.kt
@@ -2,11 +2,11 @@ package jp.pinkikki.grawup.webmvc.infrastructure.life
 
 import jp.pinkikki.grawup.webmvc.domain.life.Task
 import jp.pinkikki.grawup.webmvc.domain.life.TaskRepository
-import org.springframework.stereotype.Component
+import org.springframework.stereotype.Repository
 
-@Component
+@Repository
 class TaskRepositoryImpl(private val jpaTaskRepository: JpaTaskRepository) : TaskRepository {
     override fun save(task: Task) {
-//        jpaTaskRepository.save(TaskEntity(task))
+        jpaTaskRepository.save(TaskEntity(task.id, task.content, task.categoryId, task.targetTime, task.notification))
     }
 }
